44001b38f7c757df96025cd64359101d340605b0
Viraj Sinha <root@vsinha.com>
2021-09-15 17:36:07 -0700

    added lines to collidables

--- src/collidables.py
+++ src/collidables.py
@@ -2,17 +2,24 @@ import src.vector as vector
 
 
 class Collidables:
-    lines = []
-    circles = []
+    def __init__(self):
+        self.lines = []
+        self.circles = []
 
     def add_line(self, start, end):
         self.lines.append((start, end))
+
+    def add_line(self, line):
+        self.lines.append((line.start, line.end))
 
     def add_circle(self, origin, radius):
         self.circles.append((origin, radius))
 
     def ray_intersections(self, start, direction, max_length=None):
         intersects = []
+
+        for (s, e) in self.lines:
+            intersects += vector.intersect_ray_vector(start, direction, s, e)
 
         for (origin, radius) in self.circles:
             intersects += vector.intersect_ray_with_circle(
